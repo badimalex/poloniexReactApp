b8cdeb5a1736b2e94b8d8c2a473d398c6a815334
Badichan Dmitriy <badimalex@gmail.com>
2018-05-19 21:07:16 +0200

    [heroku] procfile

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use(
   proxy('https://api.livecoin.net')
 );
 
-app.use(express.static('dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on prot 3000');
 });
