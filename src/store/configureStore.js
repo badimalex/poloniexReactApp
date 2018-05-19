37bd60e14d30a93af02e53455166b8db40564039
Badichan Dmitriy <badimalex@gmail.com>
2018-05-19 21:45:47 +0200

    [js] fix api url

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: '/api'
 });
 
 const enhancer = compose(
